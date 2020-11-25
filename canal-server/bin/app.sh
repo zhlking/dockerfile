06b2037eb209ffd72ebc29ad3749557d761a6688
zhlking <zhlking@cnklog.com>
2020-11-25 17:05:37 +0800

    修改 canal-server

--- canal-server/bin/app.sh
+++ canal-server/bin/app.sh
@@ -5,27 +5,30 @@ source /etc/profile
 export JAVA_HOME=/usr/local/openjdk-8
 export PATH=$JAVA_HOME/bin:$PATH
 
-if [ -z "$CANAL_ADMIN_ADDR" ] ; then
-  CANAL_ADMIN_ADDR="$CANAL_ADMIN_SVC_SERVICE_HOST:$CANAL_ADMIN_SVC_SERVICE_PORT"
-fi
 
 # 获取节点主机信息
 CANAL_HOST=`hostname -s`
-CANAL_DOMAIN=`hostname -d`
 
+function server_conf_file() {
 cat << EOF > /home/admin/canal-server/conf/canal.properties
 # register ip
-canal.register.ip = ${CANAL_HOST}.${CANAL_DOMAIN}
+canal.register.ip = $CANAL_HOST
 # canal admin config
-canal.admin.manager = $CANAL_ADMIN_ADDR
+canal.admin.manager = $CANAL_ADMIN_ADDR:$CANAL_ADMIN_PORT
 canal.admin.port = 11110
-canal.admin.user = $CANAL_ADMIN_USER
-canal.admin.passwd = $CANAL_ADMIN_PASSWD
+canal.admin.user = $CANAL_SERVER_USER
+canal.admin.passwd = $CANAL_SERVER_PASSWD
 # admin auto register
 canal.admin.register.auto = true
 canal.admin.register.cluster = $CANAL_CLUSTER
 EOF
+}
+
+if [ ! -f /home/admin/canal-server/conf/canal.properties ] ; then
+    server_conf_file
+fi
+
 
 sleep 5
 
-cd /home/admin/canal-server/bin/ && gosu admin sh startup.sh 2>&1
+cd /home/admin/canal-server/bin/ && gosu admin sh ./startup.sh 2>&1
--- canal-server/bin/startup.sh
+++ canal-server/bin/startup.sh
@@ -1,44 +1,53 @@
 #!/bin/bash 
 
 current_path=`pwd`
-
-base=/home/admin/canal-server
+case "`uname`" in
+    Linux)
+		bin_abs_path=$(readlink -f $(dirname $0))
+		;;
+	*)
+		bin_abs_path=`cd $(dirname $0); pwd`
+		;;
+esac
+base=${bin_abs_path}/..
 canal_conf=$base/conf/canal.properties
 canal_local_conf=$base/conf/canal_local.properties
 logback_configurationFile=$base/conf/logback.xml
 export LANG=en_US.UTF-8
 export BASE=$base
-source /etc/profile
-export JAVA_HOME=/usr/local/openjdk-8
-export PATH=$JAVA_HOME/bin:$PATH
 
-if [ ! -d $base/logs/canal ] ; then
+
+if [ ! -d $base/logs/canal ] ; then 
 	mkdir -p $base/logs/canal
 fi
 
-JAVA="/usr/local/openjdk-8/bin/java"
-JAVA_OPTS="-server -Xms2048m -Xmx3072m -Xmn1024m -XX:SurvivorRatio=2 -XX:PermSize=96m -XX:MaxPermSize=256m -Xss256k -XX:-UseAdaptiveSizePolicy -XX:MaxTenuringThreshold=15 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError"
+## set java path
+if [ -z "$JAVA" ] ; then
+  JAVA=$(which java)
+fi
+
+str=`file -L $JAVA | grep 64-bit`
+if [ -n "$str" ]; then
+	JAVA_OPTS="-server -Xms2048m -Xmx2048m -Xmn1024m -XX:SurvivorRatio=2 -XX:PermSize=96m -XX:MaxPermSize=256m -Xss256k -XX:-UseAdaptiveSizePolicy -XX:MaxTenuringThreshold=15 -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:+HeapDumpOnOutOfMemoryError"
+else
+	JAVA_OPTS="-server -Xms1024m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:MaxPermSize=128m "
+fi
+
 JAVA_OPTS=" $JAVA_OPTS -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8"
 CANAL_OPTS="-DappName=otter-canal -Dlogback.configurationFile=$logback_configurationFile -Dcanal.conf=$canal_conf"
 
 if [ -e $canal_conf -a -e $logback_configurationFile ]
 then
-
 	for i in $base/lib/*;
 		do CLASSPATH=$i:"$CLASSPATH";
 	done
  	CLASSPATH="$base/conf:$CLASSPATH";
 
- 	echo "cd to $bin_abs_path for workaround relative path"
-  cd $base/bin/
-
 	echo LOG CONFIGURATION : $logback_configurationFile
-	echo canal conf : $canal_conf
+	echo canal conf : $canal_conf 
 	echo CLASSPATH :$CLASSPATH
-	$JAVA $JAVA_OPTS $JAVA_DEBUG_OPT $CANAL_OPTS -classpath .:$CLASSPATH com.alibaba.otter.canal.deployer.CanalLauncher 2>&1
-
-  echo "Canal-admin 启动失败"
-
+	$JAVA $JAVA_OPTS $JAVA_DEBUG_OPT $CANAL_OPTS -classpath .:$CLASSPATH com.alibaba.otter.canal.deployer.CanalLauncher 1>>/dev/null 2>&1
+	
 else 
 	echo "canal conf("$canal_conf") OR log configration file($logback_configurationFile) is not exist,please create then first!"
 fi
